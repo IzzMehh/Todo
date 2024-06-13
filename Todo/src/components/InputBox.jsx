7615b64f94293d1c65411d162398bf32b08e4dd2
Gaurav <gauravbishtyt@gmail.com>
2024-06-14 00:48:52 +0530

    Add input restriction

--- Todo/src/components/InputBox.jsx
+++ Todo/src/components/InputBox.jsx
@@ -13,7 +13,12 @@ function InputBox({handleTask}) {
             type="text" value={useTodoTitle}  name="" id="" placeholder='What do you need to do?' className='bg-[#FDD2D2] input-outline h-[62px] w-[85%] transition-all  font-text-font text-xl rounded-l-[15px] px-4 text-black' />
             <div 
             onClick={
-              ()=>{handleTask(useTodoTitle)}
+              ()=>{
+                if(useTodoTitle !== ''){
+                  handleTask(useTodoTitle)
+                  setTodoTitle('')
+                }
+              }
             }
             className='bg-[#1E212F] w-[15%] h-full rounded-r-[15px] text-xl font-text-font font-semibold cursor-pointer select-none'><span className='text-4xl relative top-3'><ion-icon name="add-outline"></ion-icon></span> Add </div>
         </div>
